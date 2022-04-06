b2101a57a9bf2cf1631edd4fbbaffa3b6bb7ff1b
s19487 <s19487@pjwstk.edu.pl>
2022-04-06 18:40:18 +0200

    initial commit

---
+++ optometry/src/main/java/com/baranskagata/optometry/OptometryApplication.java
@@ -0,0 +1,13 @@
+package com.baranskagata.optometry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptometryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptometryApplication.class, args);
+	}
+
+}
---
+++ optometry/src/test/java/com/baranskagata/optometry/OptometryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baranskagata.optometry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptometryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
